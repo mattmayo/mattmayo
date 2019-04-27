549bcbd82d4a8a2c09d469b06f90654e77a49c78
Matt Mayo <matt_mayo@icloud.com>
2019-04-27 12:22:20 -0700

    Add fluid image to MattCard

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,11 +6,12 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`
+        path: `${__dirname}/src/images/`
       }
     }
   ]
--- src/components/matt-card.jsx
+++ src/components/matt-card.jsx
@@ -1,16 +1,32 @@
 import * as React from 'react';
 import { Card, CardContent, Typography } from '@material-ui/core';
+import { graphql, useStaticQuery } from 'gatsby';
+import Img from 'gatsby-image';
 
-const MattCard = () => (
-  <div>
+const MattCard = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      file(relativePath: { eq: "matt-elvis-1.png" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `);
+
+  return (
     <Card>
       <CardContent>
+        {console.log(data.file.childImageSharp.fluid)}
+        <Img fluid={data.file.childImageSharp.fluid} alt="Matt and Elvis" />
         <Typography align={'center'} component="h3" variant="h3">
           mattmayo.com
         </Typography>
       </CardContent>
     </Card>
-  </div>
-);
+  );
+};
 
 export default MattCard;
