25fbbc6caca5f0901955be7e3e55486cb68976c7
Matt Mayo <1709814+mattmayo@users.noreply.github.com>
2020-09-11 19:17:17 -0700

    Convert JS to TS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
-import * as React from 'react';
+import React from 'react';
 import Layout from '../components/layout';
 import MattCard from '../components/matt-card';
 
-const IndexPage = () => (
+const IndexPage: React.FC = () => (
   <Layout>
     <MattCard />
   </Layout>
