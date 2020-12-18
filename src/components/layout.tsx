2c47d58c9342e5983d25fb0b39f3ff6bf98ad959
Matt Mayo <1709814+mattmayo@users.noreply.github.com>
2020-12-17 23:40:26 -0800

    Add open graphql meta

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,3 +1,4 @@
+import { graphql, useStaticQuery } from 'gatsby';
 import React from 'react';
 import { Helmet } from 'react-helmet';
 
@@ -10,15 +11,36 @@ const Layout: React.FC<LayoutPropTypes> = ({
   children,
   description,
   title,
-}: LayoutPropTypes) => (
-  <React.Fragment>
-    <Helmet>
-      <meta name="description" content={description} />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>{title}</title>
-    </Helmet>
-    {children}
-  </React.Fragment>
-);
+}: LayoutPropTypes) => {
+  const data = useStaticQuery(graphql`
+    query {
+      mattElvis: file(relativePath: { eq: "matt-elvis-1.png" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid_withWebp_tracedSVG
+            ...GatsbyImageSharpFluidLimitPresentationSize
+          }
+        }
+      }
+    }
+  `);
+
+  return (
+    <React.Fragment>
+      <Helmet>
+        <meta name="description" content={description} />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta property="og:title" content="Matt Mayo" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:image"
+          content={data.mattElvis.childImageSharp.fluid}
+        />
+        <title>{title}</title>
+      </Helmet>
+      {children}
+    </React.Fragment>
+  );
+};
 
 export default Layout;
